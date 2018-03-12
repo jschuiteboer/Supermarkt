2584e329dd405f459fce966d0e9329d21e1bada7
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-03-12 12:02:31 +0100

    added Cart.getTotal & Cart.addProduct

--- src/main/java/com/yacht/supermarkt/App.java
+++ src/main/java/com/yacht/supermarkt/App.java
@@ -1,7 +1,24 @@
 package com.yacht.supermarkt;
 
+import com.yacht.supermarkt.model.Cart;
+import com.yacht.supermarkt.model.CashRegister;
+import com.yacht.supermarkt.model.Discount;
+import com.yacht.supermarkt.model.Product;
+
+import java.math.BigDecimal;
+
 public class App {
     public static void main(String[] args) {
+        CashRegister kassa = new CashRegister();
 
+        Cart cart = new Cart();
+
+        Product a = new Product();
+        a.setName("frikandelbroodje");
+        a.setPrice(new BigDecimal("1.23"));
+        a.setDiscount(new Discount());
+        cart.addProduct(a);
+
+        System.out.print(cart.getTotal());
     }
 }
--- src/main/java/com/yacht/supermarkt/model/Cart.java
+++ src/main/java/com/yacht/supermarkt/model/Cart.java
@@ -2,9 +2,25 @@ package com.yacht.supermarkt.model;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class Cart {
-    private List<Product> contents;
+    private List<Product> contents = new ArrayList<Product>();
+
+    public void addProduct(Product product) {
+        this.getContents().add(product);
+    }
+
+    public BigDecimal getTotal() {
+        BigDecimal total = new BigDecimal(0);
+
+        for(Product product : this.getContents()) {
+            total = total.add(product.getPrice());
+        }
+
+        return total;
+    }
 }
